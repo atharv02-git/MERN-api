5c99199d8b9a5a95a7f8fe76e38c6c9670e726bb
Atharv <atharvbhandare2001@gmail.com>
2022-06-14 22:03:04 +0530

    Error and exception handling using async function

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,28 +1,36 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc    Get Goals
 // @route   Get /api/goals
 // @access  Private 
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async(req, res) => {
     res.status(200).json({ message: "Get Goals!" })
-}
+})
 
 // @desc    Set Goals
 // @route   POST /api/goals
 // @access  Private 
-const setGoals = (req, res) => {
+const setGoals = asyncHandler(async(req, res) => {
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error("Please add a text field")
+    }
     res.status(200).json({ message: "Set Goals!" })
-}
+})
+
 // @desc    update Goals
 // @route   PUT /api/goals
 // @access  Private 
-const updateGoals = (req, res) => {
+const updateGoals = asyncHandler(async(req, res) => {
     res.status(200).json({ message: `update goal ${req.params.id}` })
-}
+})
+
 // @desc    delete Goals
 // @route   Delete /api/goals
 // @access  Private 
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async(req, res) => {
     res.status(200).json({ message: `delete goal ${req.params.id}` })
-}
+})
 
 module.exports = {
     getGoals,
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const port = process.env.PORT;
 
-const port  = process.env.PORT
+const app = express();
 
-const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/goals', require('./routes/goalRoutes'))
+app.use("/api/goals", require("./routes/goalRoutes"));
 
-app.listen(port, () => console.log(`Server listening on port ${port}`))
+// error middleware
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
