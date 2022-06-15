6a2895813cd9da9d0df0220bf2efa73a6875769f
Atharv <atharvbhandare2001@gmail.com>
2022-06-15 19:47:56 +0530

    Adding user to goal model

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 
 const goalSchema = mongoose.Schema(
     {
+        // assosciating single user to many goals(populating)
+        user:{
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User', //which model to refer
+        },
         text: {
             type: 'string',
             required: [true, 'Please add a text value']
